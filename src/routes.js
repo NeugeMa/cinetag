c4ef4ba8e30470b9f69d5dbff3898807af62b11b
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-03 09:57:33 -0300

    criação da página routes.js + juntando páginas cm index

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import Inicio from "pages/Inicio";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function AppRoutes() {
+    return (
+            <BrowserRouter> 
+                <Routes> 
+                    <Route path="/" element={<Inicio />}></Route>
+                </Routes>
+            </BrowserRouter>
+    )
+}
+
+export default AppRoutes; 
+
+// Explicação código Routes
+// <BrowserRouter> está "avisando" que haverá algumas rotas dentro dele
+// <Routes> funciona como se fosse um "roteador", pois será responsável pela troca de rotas.
+    // Dentro de <Routes> estamos declarando quais rotas existirão dentro do projeto
+        // Então a rota <Route> terá o caminho path apenas com "/", pois é a página inicial, e 
+        // também declaramos qual elemento deverá imprimir quando entrarmos neste caminho, no caso a página de Inicio.
